3ff47ab511a47da72d9f35f2c31300cf9beca2ea
«movavok» <pkurishchenkoukr.net05@gmail.com>
2025-04-25 19:01:12 +0300

    Add multi-colored interface to game, update README

--- Modules/mylib.cpp
+++ Modules/mylib.cpp
@@ -18,10 +18,20 @@ void generateMap(char map[sizeWorldY][sizeWorldX]){
 
 void showMap(char map[sizeWorldY][sizeWorldX], character pl){
     system("cls");
+    string color, stop = "\033[0m";
     for (int row = 0; row < sizeWorldY; row++){
         for (int col = 0; col < sizeWorldX; col++){
-            if (pl.x == col && pl.y == row) cout << '&';
-            else cout << map[row][col];
+            switch (map[row][col]){
+            case '~':
+            case '.': color = "\033[32m"; break; // green
+            default:  color = "\033[35m";
+            }
+
+            if (pl.x == col && pl.y == row){
+                color = "\033[91m"; // yellow
+                cout << color << '&' << stop;
+            }
+            else cout << color << map[row][col] << stop;
         }
         cout << "\n";
     }
@@ -30,7 +40,7 @@ void showMap(char map[sizeWorldY][sizeWorldX], character pl){
 bool movePlayer(char map[sizeWorldY][sizeWorldX], char action, character* pl){
     int currentX = pl->x;
     int currentY = pl->y;
-    switch(action) {
+    switch (action){
     case 'w': currentY--; break;
     case 's': currentY++; break;
     case 'a': currentX--; break;
